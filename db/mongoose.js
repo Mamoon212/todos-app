82ad34c8ca9fb7e1603162a6ce32d96d1d704699
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-02 18:47:29 +0000

    deployment

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose= require("mongoose")
 mongoose.Promise=global.Promise;
-mongoose.connect("mongodb://localhost/todo-app", { useNewUrlParser: true })
+// mongoose.connect("mongodb://localhost/todo-app", { useNewUrlParser: true })
+mongoose.connect("mongodb://Mamoon:m362951847@ds145563.mlab.com:45563/todo", { useNewUrlParser: true })
 module.exports={mongoose};
