488a78b86db971e4601dc8da70546637d4580821
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-05 19:46:08 +0000

    add users/me route and authentication middleware

--- models/user.js
+++ models/user.js
@@ -1,13 +1,70 @@
 var mongoose= require("mongoose");
+var validator= require("validator");
+var jwt= require("jsonwebtoken");
+var _= require("lodash");
 
-
-var User= mongoose.model("User", {
+var UserSchema= new mongoose.Schema({
     email:{
         type: String,
         required: true,
         minlength:1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        valdiate:{
+            validator: validator.isEmail,
+            message:`{VALUE} is not a valid email`
+        }
+    },
+    password:{
+        type: String,
+        require: true,
+        minlength: 4
+    },
+    tokens:[{
+      access:{
+          type: String,
+          required: true
+      },
+      token:{
+          type: String,
+          required: true
+      }
+    }]
 });
+
+UserSchema.methods.toJSON= function(){
+    var user= this;
+    var userObj= user.toObject();
+    return _.pick(userObj, ["_id", "email"]);
+}
+
+UserSchema.methods.generateAuthToken= function(){
+    var user= this;
+    var access= "auth";
+    var token= jwt.sign({_id: user._id.toHexString(), access},'abc13').toString();
+    user.tokens= user.tokens.concat([{access,token}]);
+    return user.save().then(()=>{
+        return token;
+    });
+};
+
+UserSchema.statics.findByToken= function(token){
+    var User= this;
+    var decoded;
+    
+    try{
+        decoded= jwt.verify(token,"abc13")
+    } catch(e){
+        return Promise.reject();
+    }
+    return User.findOne({
+       "_id": decoded._id,
+       "tokens.token": token,
+       "tokens.access": "auth"
+    });
+}
+
+
+var User= mongoose.model("User", UserSchema);
 
 module.exports= {User};
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const {ObjectID}= require("mongodb");
 var {mongoose}= require("./db/mongoose");
 var {Todo}= require("./models/todo");
 var {User}= require("./models/user");
+var {authenticate}= require("./middleware/authenticate");
 
 var app=express();
 
 app.use(bodyParser.json());
  console.log(process.env.DATABASEURl);
+ 
 app.post("/todos", (req,res)=>{
     var todo= new Todo({
         text: req.body.text
@@ -86,6 +88,22 @@ app.patch("/todos/:id", (req, res)=>{
     }).catch((e)=>{
         res.status(400).send();
     })
+})
+
+app.post("/users", (req,res)=>{
+    var body= _.pick(req.body, ['email', 'password']);
+    var user= new User(body);
+     user.save().then(()=>{
+         return user.generateAuthToken();
+    }).then((token)=>{
+        res.header("x-auth", token).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+})
+
+app.get("/users/me",authenticate, (req,res)=>{
+   res.send(req.user)
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
