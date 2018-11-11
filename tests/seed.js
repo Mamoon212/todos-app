31d40749aae08d421c8f04bbcaaca45dad887817
Mahmoud Ma'moon <aftermath202@gmail.com>
2018-11-11 10:10:16 +0000

    link todos to users

--- tests/seed.js
+++ tests/seed.js
@@ -17,20 +17,23 @@ const users=[{
 },{
     _id: userTwoId,
     email: "2otta@2otta.com",
-    password: "2otta"
+    password: "2otta",
+    tokens:[{
+        access: "auth",
+        token: jwt.sign({_id: userTwoId, access: "auth"},"abc13").toString()
+        }]
 }]
 
 const todos= [{
         _id: new ObjectID(),
-        text:"1"
+        text:"1",
+        _creator: userOneId
     },{
         _id: new ObjectID(),
         text:"2",
         completed: true,
-        completedAt: 333
-    },{
-        _id: new ObjectID(),
-        text:"3"
+        completedAt: 333,
+        _creator: userTwoId
     }];
     
 const populateTodos= (done)=>{
